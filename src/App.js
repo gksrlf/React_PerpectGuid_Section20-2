b0267e207c540d7ed91da77fb5472b5b2b11c4b2
강한길 <59948163+gksrlf@users.noreply.github.com>
2022-11-28 13:40:30 +0900

    301까지 완료

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-function App() {
-  return (
-    <div>
+import { Route, Switch, Redirect } from "react-router-dom"
 
-    </div>
-  );
+import AllQuotes from "./components/pages/AllQuotes"
+import QuoteDetail from "./components/pages/QuoteDetail"
+import NewQuote from "./components/pages/NewQuote"
+import Layout from "./components/layout/Layout"
+import NotFound from "./components/pages/NotFound"
+
+function App() {
+	return (
+		<Layout>
+			<Switch>
+				<Route path="/" exact>
+					<Redirect to="/quotes" />
+				</Route>
+				<Route path="/quotes" exact>
+					<AllQuotes />
+				</Route>
+				<Route path="/quotes/:quoteId">
+					<QuoteDetail />
+				</Route>
+				<Route path="/new-quote">
+					<NewQuote />
+				</Route>
+				<Route path="*">
+					<NotFound />
+				</Route>
+			</Switch>
+		</Layout>
+	)
 }
 
-export default App;
+export default App
